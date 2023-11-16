e4b001717d013082608e64d88e115b73f410c963
pakalexey <mye604@gmail.com>
2023-11-16 14:10:38 -0500

    Pproject Entity

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -16,9 +16,13 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
     private Boolean isDeleted = false;
@@ -31,4 +35,10 @@ public class BaseEntity {
         this.lastUpdateUserId = 1L;
     }
 
+    @PreUpdate
+    public void onPreUpdate(){
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
 }
+
